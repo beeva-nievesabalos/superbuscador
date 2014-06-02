e65e3da5b5f3817b76114a09ee7ca47714209175
Ira <ira.manzano@beeva.com>
2014-06-02 18:13:12 +0200

    modificar nombre fichero de entrada

--- freeling.sh
+++ freeling.sh
@@ -1,2 +1,3 @@
-echo $* |analyzer_client localhost:50005 > ../extractorintencion/salida.txt 
+
+echo $* |analyzer_client localhost:50005 > ../extractorintencion/analisis_freeling.txt 
 python3 ../extractorintencion/intencion.py
