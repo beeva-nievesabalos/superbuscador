a80ce6de2b21d228ceb0edb3297cad55759be605
Ira <ira.manzano@beeva.com>
2014-06-02 13:06:43 +0200

    Incluir corrector

--- config.js
+++ config.js
@@ -5,10 +5,12 @@ var express = require('express')
   , routes = require('./routes')
   , path = require('path')
   , nlpquery = require('./lib/nlpquery');
-
+var corr = require("./lib/correctorteacher");
 var server =  express();
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
+var exec = require('child_process').exec;
+var sys = require('sys');
 
 // all environments
 server.set('port', process.env.PORT || 2185);
@@ -40,6 +42,47 @@ server.use(allowCrossDomain);
 // REST GENERALES
 server.get('/', routes.index);
 // similar a server.get('/ioe/addType', routes.ioe_type_add); 
+
+server.set('title', 'Application Title');
+
+server.get('/superbuscador/:name', function(req, res) {
+  res.type('application/json');
+  var cadena = req.params.name;
+  console.log("Parameter: " + cadena);
+ 
+  respuesta = new Object();
+  respuesta.sentencia = cadena;
+
+  var command = "./freeling.sh "
+  console.log("\ncandena entrada: " + cadena)
+
+  corr.corregir(cadena,function (resultado){
+    console.log("cadena corregida: " + resultado)
+    respuesta.correccion=resultado
+    //Para ejecutar freeling con la cadena que me mandan...
+    //./freeling.sh 'blabla blabala'
+    child = exec(command + "'"+resultado+"'", function (error, stdout, stderr) {
+      if (error !== null) {
+        console.log('exec error: ' + error);
+        respuesta.intencion = stderr;
+        sys.print('stderr: ' + stderr);
+      }
+      else {
+        // no error:
+        // Aquí debería sacar los Verbos y los sustantivos
+        stdout = stdout.replace('\n','').trim()
+        sys.print('stdout: ' + stdout + 'acabado');
+        respuesta.intencion= stdout;
+      }
+      res.type('application/json');
+      //res.send(stdout);
+      res.json(respuesta);
+    });
+  });
+
+
+});
+
 server.post('/', nlpquery.query);
 
 module.exports = server;
---
+++ freeling.sh
@@ -0,0 +1,2 @@
+echo $* |analyzer_client localhost:50005 > /home/ira/Desktop/myfreeling/APIs/python/salida.txt 
+python3 /home/ira/Desktop/myfreeling/APIs/python/intencion.py
