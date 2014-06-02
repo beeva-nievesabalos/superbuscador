d079cc419a6427c3096c404b13e24b7c0e78afc1
Ira <ira.manzano@beeva.com>
2014-06-02 15:31:53 +0200

    Modificada ruta

--- freeling.sh
+++ freeling.sh
@@ -1,2 +1,2 @@
-echo $* |analyzer_client localhost:50005 > /home/ira/Desktop/myfreeling/APIs/python/salida.txt 
-python3 /home/ira/Desktop/myfreeling/APIs/python/intencion.py
+echo $* |analyzer_client localhost:50005 > ../freeling/salida.txt 
+python3 ../freeling/intencion.py
