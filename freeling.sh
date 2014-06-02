04c56c99b6cba416e2001e425a08c31fff002332
Ira <ira.manzano@beeva.com>
2014-06-02 17:59:57 +0200

    modificacion rutas

--- freeling.sh
+++ freeling.sh
@@ -1,2 +1,2 @@
-echo $* |analyzer_client localhost:50005 > ../freeling/salida.txt 
-python3 ../freeling/intencion.py
+echo $* |analyzer_client localhost:50005 > ../extractorintencion/salida.txt 
+python3 ../extractorintencion/intencion.py
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'BBVA' , information: ''});
+  res.render('index', { title: 'Superbuscador BBVA' , information: ''});
 };
 
