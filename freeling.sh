ad80ba9b3d69ed36cab9b49e8bc5a5e1eeeee88d
Ira <ira.manzano@beeva.com>
2014-06-03 16:26:44 +0200

    modificar nombre repo

--- freeling.sh
+++ freeling.sh
@@ -1,3 +1,2 @@
-export FREELING_PYTHON=/home/ira/Desktop/myfreeling
 echo $* |analyzer_client localhost:50005 > ../extractorIntencionSuperbuscador/analisis_freeling.txt 
 python3 ../extractorIntencionSuperbuscador/intencion.py
